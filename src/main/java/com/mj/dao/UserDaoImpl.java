dc1305ef264b31fcc05df08d9cdfc31af01683df
JaeJun Yang <38521736+RetroTV@users.noreply.github.com>
2019-04-12 18:19:54 +0900

    fix : 오타 수정
    @Repository("UserDao") -> @Repository("UserDao")

--- src/main/java/com/mj/dao/UserDaoImpl.java
+++ src/main/java/com/mj/dao/UserDaoImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import com.mj.mapper.UserMapper;
 import com.mj.vo.User;
 
-@Repository("UserDao")
+@Repository("userDao")
 public class UserDaoImpl implements UserDao {
 
 	@Autowired
